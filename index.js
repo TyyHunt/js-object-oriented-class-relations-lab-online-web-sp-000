5fe79c7eae2d52d559b0a2fe121b5c765fd1cf29
Tyler Hunt <tyhunt04@gmail.com>
2019-06-12 16:34:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ class Driver {
 
 class Passenger {
   constructor(name) {
-    this.name = name
-    this.id = ++passengerId
+    this.name = name;
+    this.id = ++passengerId;
 
-    store.passengers.push(this)
+    store.passengers.push(this);
   };
 };
