f7db3180a0ffca21554c465226d115e344e8d96a
Tyler Hunt <tyhunt04@gmail.com>
2019-06-12 16:32:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+let store = { drivers: [],};
+
 let driverId = 0;
 
 class Driver {
   constructor(name) {
     this.name = name;
     this.id = ++driverId;
+
+    store.drivers.push(this);
   }
 }
